87f8720231081c26fe2817ef548bc065cd828d26
Yue Qin <747929791@qq.com>
2020-10-01 20:53:47 +0800

    fix a bug at network.py

--- network.py
+++ network.py
@@ -15,6 +15,8 @@ def handle_client(client_socket):
     """
     # 获取客户端请求数据
     request_data = client_socket.recv(1024)
+    if len(request_data) == 0:
+        return
     print("request data:", request_data)
     request_lines = request_data.splitlines()
     # 解析请求报文
@@ -27,25 +29,23 @@ def handle_client(client_socket):
         file_name = "/index.html"
 
     md5 = file_name[1:]
+    response_headers = "Computer Science Foundation Homework Server\r\n"
     if len(md5) != 32 or not md5.isalnum():
         response_start_line = "HTTP/1.1 404 Not Found\r\n"
-        response_headers = "Server: My server\r\n"
-        response_body = "The file is not found!"
+        response_body = "404 not found!"
     else:
         # 打开文件，读取内容
         try:
             file = open(os.path.join(HTML_ROOT_DIR, md5), "rb")
         except IOError:
             response_start_line = "HTTP/1.1 404 Not Found\r\n"
-            response_headers = "Server: My server\r\n"
-            response_body = "The file is not found!"
+            response_body = "404 not found!"
         else:
             file_data = file.read()
             file.close()
 
             # 构造响应数据
             response_start_line = "HTTP/1.1 200 OK\r\n"
-            response_headers = "Computer Science Foundation Homework Server\r\n"
             response_body = '<html><head><meta http-equiv="content-type" content="txt/html; charset=utf-8" /><title>Homework</title></head><body><pre>' + \
                 file_data.decode("utf-8") + '</pre></body></html>'
 
